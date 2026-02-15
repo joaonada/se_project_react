66d248d47e93793b452a90a1a2f251d62c69ce15
“Joao <joaonada@icloud.com>
2026-02-15 13:18:28 -0500

    Final Stage Sprint 11

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,26 +14,27 @@ function Header({ handleAddClick, weatherData }) {
   return (
     <header className="header">
       <div className="header__container">
-        {/* todo - link home page */}
-      <img className="header__logo" src={logo} alt="WTWR logo" />
-      <p className="header__date-and-location">
-        {currentDate}, {weatherData.city}
-      </p>
+        <NavLink to="/">
+          <img className="header__logo" src={logo} alt="WTWR logo" />
+        </NavLink>      
+        <p className="header__date-and-location">
+          {currentDate}, {weatherData.city}
+        </p>
       </div>
-       <ToggleSwitch />
+      <ToggleSwitch />
       <button
         onClick={handleAddClick}
         type="button"
         className="header__add-clothes-btn"
-      > 
+      >
         + Add Clothes
       </button>
-     
+
       <NavLink className="header__nav-link" to="/profile">
-      <div className="header__user-container">
-        <p className="header__username">Terrence Tegegne</p>
-        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
-      </div>
+        <div className="header__user-container">
+          <p className="header__username">Terrence Tegegne</p>
+          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+        </div>
       </NavLink>
     </header>
   );
